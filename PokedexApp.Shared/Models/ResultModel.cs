bdeda9072562f381be154a711ad779e2b535da24
Nugget1368 <rneg1@hotmail.com>
2024-03-31 14:57:36 +0200

    ResultModel
    Updated Service and controller with a ResultModel containing a bool property Success which will be true or false depending on the result. Property Success kan be used to avoid Null References when call to API has failed

---
+++ PokedexApp.Shared/Models/ResultModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PokedexApp.Shared.Models
+{
+	public class ResultModel<T>
+	{
+		public bool Success { get; set; }
+		public string ResultMessage { get; set; } = String.Empty;
+		public T? Result { get; set; }
+	}
+}
